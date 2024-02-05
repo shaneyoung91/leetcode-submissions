64111587dcf4614811f11e032c1558df8be1d411
Shane Young <134543682+shaneyoung91@users.noreply.github.com>
2024-02-05 14:05:35 -0800

    Time: 139 ms (81.75%), Space: 17.1 MB (69.03%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
@@ -1,27 +1,16 @@
 class Solution:
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
         
-        # append new arrays to answer 
-        # iterate through num1 - if value is also in nums2, add value to new array
-        # append updated array to answer
-        # repeat for nums2 array
+        # create sets for nums1 and nums2
+        # assign variable for the difference in set_nums1 and set_nums2
+            # convert to a list
+        # repeat for difference in set_nums2 and set_nums1
+        # return both variables as subarrays
         
-        answer = []
-        dist_nums1 = []
-        dist_nums2 = []
+        set_nums1 = set(nums1)
+        set_nums2 = set(nums2)
         
-        for num in nums1:
-            if num not in nums2:
-                if num not in dist_nums1:
-                    dist_nums1.append(num)
+        dist_nums1 = list(set_nums1 - set_nums2)
+        dist_nums2 = list(set_nums2 - set_nums1)
         
-        answer.append(dist_nums1)
-        
-        for num in nums2:
-            if num not in nums1:
-                if num not in dist_nums2:
-                    dist_nums2.append(num)  
-        
-        answer.append(dist_nums2)
-        
-        return answer
+        return [dist_nums1, dist_nums2]
